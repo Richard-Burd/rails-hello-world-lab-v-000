f976905a3e1031807c55da772eca211783ebacfa
Richard Burd <richardburdx@yahoo.com>
2018-03-15 16:13:15 +0000

    automatically backed up by learn

--- spec/controllers/posters_controller_spec.rb
+++ spec/controllers/posters_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PostersController, type: :controller do
   describe "GET #burdaction" do   # note that this is the name of the actual action, not the URL
     it "returns http success" do
-      get :burdaction             # note that this is the name of the actual action, not the URL
+      get :burd                   # note that this is the name of the actual action, not the URL
       expect(response).to have_http_status(:success)
     end
   end
